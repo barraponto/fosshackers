04d7f31746af091c18788a4c58004394905c1578
Capi Etheriel <barraponto@gmail.com>
2015-06-20 05:15:46 -0300

    added FosshackersItemLoader

--- fosshackers/items.py
+++ fosshackers/items.py
@@ -6,9 +6,15 @@
 # http://doc.scrapy.org/en/latest/topics/items.html
 
 import scrapy
+from scrapy.loader import ItemLoader
+from scrapy.loader.processors import TakeFirst
 
 
 class FosshackersItem(scrapy.Item):
     id_ = scrapy.Field()
     type_ = scrapy.Field()
 
+
+class FosshackersItemLoader(ItemLoader):
+    default_item_class = FosshackersItem
+    default_output_processor = TakeFirst()
