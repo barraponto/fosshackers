bf661972f54502a8e34b9d97a2020e238327cdeb
Capi Etheriel <barraponto@gmail.com>
2015-06-20 04:44:10 -0300

    [djangopatchers] added specific parse callback

--- fosshackers/spiders/djangopatchers.py
+++ fosshackers/spiders/djangopatchers.py
@@ -13,10 +13,14 @@ class DjangopatchersSpider(scrapy.Spider):
 
     def parse(self, response):
         # Get issue data from the main table
-        for issue in response.css('.tickets.listing tbody tr'):
-            yield {'id': issue.css('.id a::text').extract_first(),
-                   'type': issue.css('.type::text').extract_first().strip()}
+        for issue_url in response.css('.tickets.listing tbody .id a::attr("href")').extract():
+            yield scrapy.Request(response.urljoin(issue_url),
+                                 callback=self.parse_issue)
 
         # Get the other pages
         for page_url in response.css('.paging a::attr("href")').extract():
             yield scrapy.Request(response.urljoin(page_url))
+
+    def parse_issue(self, response):
+        return {'id': response.css('h2 > a::text').extract_first(),
+                'type': response.css('.trac-type a::text').extract_first()}
