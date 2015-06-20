5ba027955bf2af913cc1f37b2a813fe1973a968f
Capi Etheriel <barraponto@gmail.com>
2015-06-20 04:58:24 -0300

    [djangopatcherscrawl] ported djangopatchers logic to CrawlSpider

--- fosshackers/spiders/djangopatcherscrawl.py
+++ fosshackers/spiders/djangopatcherscrawl.py
@@ -1,5 +1,4 @@
 # -*- coding: utf-8 -*-
-import scrapy
 from scrapy.linkextractors import LinkExtractor
 from scrapy.spiders import CrawlSpider, Rule
 
@@ -9,15 +8,26 @@ from fosshackers.items import FosshackersItem
 class DjangopatcherscrawlSpider(CrawlSpider):
     name = 'djangopatcherscrawl'
     allowed_domains = ['code.djangoproject.com']
-    start_urls = ['http://www.code.djangoproject.com/']
+    start_urls = [
+        'https://code.djangoproject.com/query'
+        '?status=assigned&status=closed&status=new'
+        '&has_patch=1&desc=1&order=id',
+    ]
 
     rules = (
-        Rule(LinkExtractor(allow=r'Items/'), callback='parse_item', follow=True),
+        Rule(LinkExtractor(restrict_css='.paging a'),
+             follow=True),
+        Rule(LinkExtractor(restrict_css='.tickets.listing tbody .id a'),
+             callback='parse_issue', follow=False),
     )
 
-    def parse_item(self, response):
-        i = FosshackersItem()
-        #i['domain_id'] = response.xpath('//input[@id="sid"]/@value').extract()
-        #i['name'] = response.xpath('//div[@id="name"]').extract()
-        #i['description'] = response.xpath('//div[@id="description"]').extract()
-        return i
+    def parse_issue(self, response):
+        return {'id': response.css('h2 > a::text').extract_first(),
+                'type': response.css('.trac-type a::text').extract_first()}
+
+    # def parse_item(self, response):
+    #     i = FosshackersItem()
+    #     #i['domain_id'] = response.xpath('//input[@id="sid"]/@value').extract()
+    #     #i['name'] = response.xpath('//div[@id="name"]').extract()
+    #     #i['description'] = response.xpath('//div[@id="description"]').extract()
+    #     return i
